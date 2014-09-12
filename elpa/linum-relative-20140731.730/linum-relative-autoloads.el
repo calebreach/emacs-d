5ca1c618ffd9459ea9b0c98c6b9e515eda6a00c2
Caleb Reach <caleb@acceliva.com>
2014-09-12 11:57:26 -0700

    added elpa

---
+++ elpa/linum-relative-20140731.730/linum-relative-autoloads.el
@@ -0,0 +1,18 @@
+;;; linum-relative-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("linum-relative-pkg.el" "linum-relative.el")
+;;;;;;  (21517 38059 205953 0))
+
+;;;***
+
+(provide 'linum-relative-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; linum-relative-autoloads.el ends here
